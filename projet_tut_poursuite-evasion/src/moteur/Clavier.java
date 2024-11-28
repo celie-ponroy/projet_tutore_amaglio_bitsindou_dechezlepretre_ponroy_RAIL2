d6d807a311f4de1a94b6549b8eefa701ed6614f6
Célie Ponroy <celie.ponroy@laposte.net>
2024-11-28 11:14:30 +0100

    Modif des touches du clavier

--- projet_tut_poursuite-evasion/src/moteur/Clavier.java
+++ projet_tut_poursuite-evasion/src/moteur/Clavier.java
@@ -20,42 +20,51 @@ public class Clavier {
         switch (event.getCode()) {
             // si en haut a gauche
             case A:
+            case NUMPAD7:
                 this.diagHG = true;
                 break;
             // si en haut a droite
             case E:
+            case NUMPAD9:
                 this.diagHD = true;
                 break;
 
             // si touche haut
             case Z:
+            case NUMPAD8:
                 this.haut = true;
                 break;
 
             // si touche gauche
             case Q:
+            case NUMPAD4:
                 this.gauche = true;
                 break;
 
             // si touche droite
             case D:
+            case NUMPAD6:
                 this.droite = true;
                 break;
 
             //si touche neutre
             case S:
+            case NUMPAD5:
                 this.neutre = true;
                 break;
             // si en bas a gauche
             case W:
+            case NUMPAD1:
                 this.diagBG = true;
                 break;
             // si en bas a droite
             case C:
+            case NUMPAD3:
                 this.diagBD = true;
                 break;
             // si en bas
             case X:
+            case NUMPAD2:
                 this.bas = true;
                 break;
         }
@@ -73,42 +82,51 @@ public class Clavier {
 
             // si en haut a gauche
             case A:
+            case NUMPAD7:
                 this.diagHG = false;
                 break;
             // si en haut a droite
             case E:
+            case NUMPAD9:
                 this.diagHD = false;
                 break;
 
             // si touche haut
             case Z:
+            case NUMPAD8:
                 this.haut = false;
                 break;
 
             // si touche gauche
             case Q:
+            case NUMPAD4:
                 this.gauche = false;
                 break;
 
             // si touche droite
             case D:
+            case NUMPAD6:
                 this.droite = false;
                 break;
 
             //si touche neutre
             case S:
+            case NUMPAD5:
                 this.neutre = false;
                 break;
             // si en bas a gauche
             case W:
+            case NUMPAD1:
                 this.diagBG = false;
                 break;
             // si en bas a droite
             case C:
+            case NUMPAD3:
                 this.diagBD = false;
                 break;
             // si en bas
             case X:
+            case NUMPAD2:
                 this.bas = false;
                 break;
         }

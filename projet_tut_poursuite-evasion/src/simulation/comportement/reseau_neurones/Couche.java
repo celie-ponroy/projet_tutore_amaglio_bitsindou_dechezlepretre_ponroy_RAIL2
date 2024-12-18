13f6a1df888a39986ee2ccc2b7c36472419fe123
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-12-18 08:39:00 +0100

    rebasage arbre

--- projet_tut_poursuite-evasion/src/simulation/comportement/reseau_neurones/Couche.java
+++ projet_tut_poursuite-evasion/src/simulation/comportement/reseau_neurones/Couche.java
@@ -2,32 +2,18 @@ package simulation.comportement.reseau_neurones;
 
 class Couche {
     public Neurone Neurons[];
-<<<<<<< HEAD
     public int Length;
-=======
-    public int 	  Length;
->>>>>>> 1279a4b (rebasage arbre)
 
     /**
      * Couche de Neurones
      *
-<<<<<<< HEAD
      * @param l    Taille de la couche
      * @param prev Taille de la couche précédente
-=======
-     * @param l     Taille de la couche
-     * @param prev  Taille de la couche précédente
->>>>>>> 1279a4b (rebasage arbre)
      */
     public Couche(int l, int prev) {
         Length = l;
         Neurons = new Neurone[l];
-
-<<<<<<< HEAD
         for (int j = 0; j < Length; j++)
-=======
-        for(int j = 0; j < Length; j++)
->>>>>>> 1279a4b (rebasage arbre)
             Neurons[j] = new Neurone(prev);
     }
 }

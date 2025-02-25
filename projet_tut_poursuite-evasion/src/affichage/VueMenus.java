0e39699f5815c96cf53b34b66c30a2465e5dd6a7
Célie Ponroy <celie.ponroy@laposte.net>
2025-02-25 11:12:36 +0100

    fin informations ia

--- projet_tut_poursuite-evasion/src/affichage/VueMenus.java
+++ projet_tut_poursuite-evasion/src/affichage/VueMenus.java
@@ -4,6 +4,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -344,26 +346,26 @@ public class VueMenus {
             }
         });
         //ajout bouton informatif
-        Button info = new Button("Informations");
-        info.setPrefSize(150, 50);
+        Button info = InformationsIa.getButtonInfo();
+
         info.setOnAction(e -> {
             //on lance un popup
-            Alert alert;
             if(choixPersonnage == "Prisonnier") {
-                alert = InformationsIa.getAlertGardien();
+                InformationsIa.popUpGardien();
 
             }else {
-                alert = InformationsIa.getAlertPrisonnier();
+                InformationsIa.popUpPrisonnier();
             }
-            alert.setResizable(true);
-            alert.showAndWait();
         });
-
+        HBox buttonBox2 = new HBox();
+        buttonBox2.setSpacing(20);
+        buttonBox2.setAlignment(Pos.CENTER);
+        buttonBox2.getChildren().addAll(okButton, info);
         //Ajout bouton retour
         Button retour = retourBtn();
 
         //Ajout des éléments à la scene principale
-        root.getChildren().addAll(title, buttonBox, okButton, info, retour);
+        root.getChildren().addAll(title, buttonBox, buttonBox2, retour);
 
         //Affichage de la scene et changement du titre de la fenêtre
         setScene(scene, "Choix de la difficulté de l'IA adverse");
--- projet_tut_poursuite-evasion/src/affichage/VueMenusNonInteractive.java
+++ projet_tut_poursuite-evasion/src/affichage/VueMenusNonInteractive.java
@@ -183,19 +183,17 @@ public class VueMenusNonInteractive {
                 }
             }
         });
-        //ajout bouton informatif
-        Button info = new Button("Informations");
-        info.setPrefSize(150, 50);
-        info.setOnAction(e -> {
-            //on lance un popup
-            Alert alert;
-            alert = InformationsIa.getAlertNonInteractif();
-            alert.setResizable(true);
-            alert.showAndWait();
-        });
 
+        //ajout bouton informatif
+        Button info = InformationsIa.getButtonInfo();
+        info.setOnAction(e -> {
+            InformationsIa.popUpNonInteractif();
+        });
+        HBox hBoxButtons = new HBox();
+        hBoxButtons.getChildren().addAll(okButton ,info);
+        hBoxButtons.setAlignment(Pos.CENTER);
         // Ajout des éléments à la racine
-        root.getChildren().addAll(title, container, okButton,info,retour);
+        root.getChildren().addAll(title, container, hBoxButtons,retour);
 
         // Création et affichage de la scène
         primaryStage.setScene(scene);

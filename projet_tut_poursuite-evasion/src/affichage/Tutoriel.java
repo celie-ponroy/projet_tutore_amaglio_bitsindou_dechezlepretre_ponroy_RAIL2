bc82a27063329c5cf0c86724c3de53a400651638
Célie Ponroy <celie.ponroy@laposte.net>
2025-02-25 14:05:13 +0100

    ajout classe Tutoriel

---
+++ projet_tut_poursuite-evasion/src/affichage/Tutoriel.java
@@ -0,0 +1,9 @@
+package affichage;
+
+/**
+ * Affiche un tutoriel du mode interactif.
+ */
+public class Tutoriel {
+    // partie avec les controles et les déplacements
+    // partie qui explique les cases 
+}
